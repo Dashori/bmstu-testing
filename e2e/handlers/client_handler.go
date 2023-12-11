4c55bda55bbefe03fd295b75f50bd33d1ee757da
Chepigo Darya <chepigods@student.bmstu.ru>
2023-12-12 01:55:06 +0300

    lab_05 | try to fix address with dockerfile

--- e2e/handlers/client_handler.go
+++ e2e/handlers/client_handler.go
@@ -9,7 +9,7 @@ import (
 )
 
 const port = "8080"
-const address = "docker"
+const address = "localhost"
 
 func DoRequest(client *http.Client, request *http.Request) (*http.Response, error) {
 	response, err := client.Do(request)
