79ae6469778d75a6a3a09fb440b5c0755ab410ec
Chepigo Darya <chepigods@student.bmstu.ru>
2023-12-13 00:41:45 +0300

    lab_05 | fix all!

--- e2e/main.go
+++ e2e/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"consoleApp/client_controller"
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 )
 
 func main() {
@@ -15,32 +12,5 @@ func main() {
 	err := client_controller.ClientTest(client)
 	if err != nil {
 		fmt.Println(err)
-	}
-
-	defer func() {
-		if r := recover(); r != nil {
-			err, _ := json.Marshal(r)
-			_ = os.WriteFile("trace.json", err, os.ModePerm)
-		}
-	}()
-
-	req, err := http.NewRequest(http.MethodGet, "http://localhost:16686/api/traces?service=backend", nil)
-	if err != nil {
-		panic(err)
-	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		panic("incorrect status " + resp.Status)
-	}
-	jsonBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	err = os.WriteFile("trace.json", jsonBody, os.ModePerm)
-	if err != nil {
-		panic(err)
 	}
 }
